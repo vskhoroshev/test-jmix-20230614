f4aa651ac6fb51f00dfc3b7edfb6da48018de288
Vadim Khoroshev <vs.khoroshev@gmail.com>
2023-06-14 16:23:56 +0300

    init: инициализировать проект

---
+++ src/test/java/io/vskhoroshev/testjmix/TestJmix20230614ApplicationTests.java
@@ -0,0 +1,12 @@
+package io.vskhoroshev.testjmix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJmix20230614ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
